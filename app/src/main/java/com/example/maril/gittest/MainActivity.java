860c035cfc0ff3f41e301c08f6341e06ee8f5e12
marilenabaldi <marilena.baldi@studenti.unina2.it>
2017-04-03 20:27:59 +0200

    dichiarazione interi

--- app/src/main/java/com/example/maril/gittest/MainActivity.java
+++ app/src/main/java/com/example/maril/gittest/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//provaprovaprova
+    public int a;
+    public int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
